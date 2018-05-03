928410ca3190498b549ea985df60eefa0cbd5b3e
jin woo <kki5629@naver.com>
2018-05-03 11:28:56 +0000

    fourth session

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,4 +19,21 @@ class ArticlesController < ApplicationController
     @article=Article.find(params[:id])
 
   end
+  def destroy
+    article = Article.find(params[:id])
+    article.destroy
+    
+    redirect_to articles_path
+  end
+  def edit
+    @article = Article.find(params[:id])
+  end
+  def update
+    @article = Article.find(params[:id])
+    @article.update(params.require(:article).permit(:title, :content))
+    
+    redirect_to @article
+  end
+  
+  
 end
